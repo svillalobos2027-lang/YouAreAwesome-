b1e2e0883e5df314f15916cd63afc760824ada09
Stephanie Villalobos <svillalobos2027@gmail.com>
2025-11-14 16:42:58 -0800

    added images and cases statements on novemeber 14

--- YouAreAwesome!/ContentView.swift
+++ YouAreAwesome!/ContentView.swift
@@ -9,41 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var imageString = ""
+    @State private var imageName = ""
     var body: some View {
         VStack {
             Spacer()
             
               
-            Image(systemName: imageString)
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
               
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
             
             Spacer()
             
             Button("Press Me!") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
-                
-                if message == message1{
-                    message = message2
-                    imageString = imageString2
-                } else {
-                    message = message1
-                    imageString = imageString1
-                }
+            
+                message = (message == message1 ?  message2 : message1 )
+                imageName = ( imageName == "image0" ? "peacefuldog" : "image0")
                 
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
             
             .padding()
             
