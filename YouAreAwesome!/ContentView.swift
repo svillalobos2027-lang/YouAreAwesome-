2ec9f27983cf78b369c684f4b32cc07f21d3d935
Stephanie Villalobos <svillalobos2027@gmail.com>
2025-11-17 11:48:23 -0800

    Added images and new conditional

--- YouAreAwesome!/ContentView.swift
+++ YouAreAwesome!/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var imageNumber = 0
     var body: some View {
         VStack {
             Spacer()
@@ -28,13 +29,29 @@ struct ContentView: View {
             
             Spacer()
             
-            Button("Press Me!") {
+            Button("Show Message") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
+                let message3 = "Fabulous? That's You!"
             
-                message = (message == message1 ?  message2 : message1 )
-                imageName = ( imageName == "image0" ? "peacefuldog" : "image0")
+                if message == message1 {
+                    message = message2
+                } else if message == message2 {
+                    message = message3
+                } else {
+                    message = message1
+                }
+              //TODO: - Update the imageName Variable -
+                imageName = "image\(imageNumber)"
+
+                imageNumber += 1
                 
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+                
+            
+            
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
