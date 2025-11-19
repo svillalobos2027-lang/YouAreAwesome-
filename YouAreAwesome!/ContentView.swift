392f31a56209d19e7b82460edfd5172ecee01a96
Stephanie Villalobos <svillalobos2027@gmail.com>
2025-11-19 11:47:08 -0800

    11/19/25

--- YouAreAwesome!/ContentView.swift
+++ YouAreAwesome!/ContentView.swift
@@ -15,7 +15,14 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                . multilineTextAlignment(.center)
+                . minimumScaleFactor(0.5)
+                .frame(height: 100)
+
             
               
             Image(imageName)
@@ -23,22 +30,19 @@ struct ContentView: View {
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
+                .animation(<#T##animation: Animation?##Animation?#>, value: <#T##Equatable#>)
               
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                . multilineTextAlignment(.center)
             
             Spacer()
             
             Button("Show Message") {
                 let messages = ["You Are Awesome!",
+                                "Gadzooks my friend! I am astonished at how utterly magnificant you are!",
+                                "When the Genius Bar Needs Help, They Call You!",
                                 "You Are Great!",
                                 "You Are Fantastic!",
                                 "Fabulous? That's You!",
-                                "You Make Me Smile!",
-                                "When the Genius Bar Needs Help, They Call You!"]
+                                "You Make Me Smile!"]
                 message = messages[messageNumber]
                 messageNumber = messageNumber + 1
                 if messageNumber == messages.count {
